803e3fc1a1f0ca0d379e77a76f7ee27de6f6edf6
sanco <sanco@LAPTOP-238J0HVD>
2022-03-16 19:07:46 +0100

    Creacion das seguintes clases

--- ArtigoFactura/src/main/java/com/mycompany/artigofactura/ArtigoFactura.java
+++ ArtigoFactura/src/main/java/com/mycompany/artigofactura/ArtigoFactura.java
@@ -7,18 +7,64 @@ package com.mycompany.artigofactura;
  */
 public class ArtigoFactura {
     
-    private int valorActual;
+ private String id;
+ private String descripcion;
+ private int cantide;
+ private double prezoUnitario;
+ 
+ 
     
-    public ArtigoFactura (int valorInicial){
-        this.valorActual = valorInicial;
+    public ArtigoFactura (String id, String descripcion, int cantidade, double prezoUnitario ){
+        this.cantide = cantidade;
+        this.descripcion = descripcion;
+        this.id = id;
+        this.prezoUnitario = prezoUnitario;
     }
     
     public String toString(){
         
-        String valor =  String.valueOf(valorActual);
-        return valor;
+        
+        return descripcion;
     }
     
+   public double obterPrezo(){
+       
+       return prezoUnitario;
+   }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getCantide() {
+        return cantide;
+    }
+
+    public void setCantide(int cantide) {
+        this.cantide = cantide;
+    }
+
+    public double getPrezoUnitario() {
+        return prezoUnitario;
+    }
+
+    public void setPrezoUnitario(double prezoUnitario) {
+        this.prezoUnitario = prezoUnitario;
+    }
+   
+   
     
     
 }
